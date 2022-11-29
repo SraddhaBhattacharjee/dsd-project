4284231e3774baf845e0842ffd452dc31435d8a4
Sadman Shawmik <hello@sadmansh.com>
2022-11-29 15:59:05 -0500

    Add workflow and cloud function trigger

---
+++ trigger/main.py
@@ -0,0 +1,21 @@
+import json
+import google.auth
+from google.auth.transport.requests import AuthorizedSession
+
+def onNewFile(event, context):
+
+    print('Event ID: {}'.format(context.event_id))
+    print('Event type: {}'.format(context.event_type))
+    print('Bucket: {}'.format(event['bucket']))
+    print('File: {}'.format(event['name']))
+
+    scoped_credentials, project = google.auth.default(
+        scopes=['https://www.googleapis.com/auth/cloud-platform'])
+    authed_session = AuthorizedSession(scoped_credentials)
+
+    URL = 'https://workflowexecutions.googleapis.com/v1/projects/dsd-fall22/locations/northamerica-northeast1/workflows/load-workflow/executions'
+    file_id_dict = { 'bucket': '{}'.format(event['bucket']), 'object': '{}'.format(event['name']) }
+    PARAMS = { 'argument' : json.dumps(file_id_dict) }
+    response = authed_session.post(url=URL, json=PARAMS)
+
+    print(response)
